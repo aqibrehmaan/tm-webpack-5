a56e6f3230e22b2aa21275a25a845617d2d2ad6d
Aqib Rehman <aqibr777@gmail.com>
2022-06-18 01:27:52 +0500

    Loaders & Sass Compiling

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -9,5 +10,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin
+    ]
 }
